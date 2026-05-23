db4d34881348a26a21b06865378920a8ced8557e
Anik Adhikari <anikadhikari04@gmail.com>
2026-05-23 21:38:19 +0530

    fix: Vercel build warning and enforce container centering

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,3 @@
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
