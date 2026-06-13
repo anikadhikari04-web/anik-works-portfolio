0320963e44e31df6cd9cd4131f7ec8e386828380
Anik Adhikari <anikadhikari04@gmail.com>
2026-06-13 11:11:54 +0530

    Update shadows to navy and purple, improve scroll performance

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -48,12 +48,12 @@ function App() {
         <ScrollToTop />
 
         <div className="flex flex-col min-h-screen bg-background text-foreground font-sans selection:bg-primary selection:text-white relative overflow-x-hidden">
-          
+
           {/* Global Smooth Ambient Glow */}
-          <div className="fixed inset-0 z-0 pointer-events-none">
-            <div className="absolute top-[-20%] left-[-10%] w-[60vw] h-[60vw] rounded-full bg-[#8A2BE2]/5 blur-[150px]" />
-            <div className="absolute top-[40%] right-[-10%] w-[50vw] h-[50vw] rounded-full bg-blue-600/5 blur-[150px]" />
-            <div className="absolute bottom-[-20%] left-[20%] w-[60vw] h-[60vw] rounded-full bg-[#9400D3]/5 blur-[150px]" />
+          <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden">
+            <div className="absolute top-[-20%] left-[-10%] w-[60vw] h-[60vw] rounded-full bg-[#8A2BE2]/5 blur-[150px] transform-gpu will-change-transform" />
+            <div className="absolute top-[40%] right-[-10%] w-[50vw] h-[50vw] rounded-full bg-blue-600/5 blur-[150px] transform-gpu will-change-transform" />
+            <div className="absolute bottom-[-20%] left-[20%] w-[60vw] h-[60vw] rounded-full bg-[#9400D3]/5 blur-[150px] transform-gpu will-change-transform" />
           </div>
 
           <Navbar />
